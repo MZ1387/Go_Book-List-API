94203aa4945f0adc4b625b1a906df2c2121f18f0
Mario Zamora <mariozamora@Marios-MacBook-Pro-2.local>
2018-10-17 15:36:04 -0300

    Update delete single book route

--- main.go
+++ main.go
@@ -114,5 +114,12 @@ func updateBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func removeBooks(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	result, err := db.Exec("DELETE from books where id=$1", params["id"])
+	logFatal(err)
 
+	rowsDeleted, err := result.RowsAffected()
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(rowsDeleted)
 }
